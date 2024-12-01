6eb34c2819f66967e42c17ea2f7390796309c9b5
Erhan <Erhan@DESKTOP-RE6D6MA>
2024-12-02 00:40:51 +0300

    .

--- bsStoreApp/Entities/RequestFeatures/BookParameters.cs
+++ bsStoreApp/Entities/RequestFeatures/BookParameters.cs
@@ -6,7 +6,10 @@
         public uint MinPrice { get; set; }
         public uint MaxPrice { get; set; } = 1000;
         public bool ValidPriceRange => MaxPrice > MinPrice;
-
         public String? SearchTerm { get; set; }
+        public BookParameters()
+        {
+            OrderBy = "id";
+        }
     }
 }
--- bsStoreApp/Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ bsStoreApp/Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extensions
 {
@@ -26,5 +28,21 @@ namespace Repositories.EFCore.Extensions
                 .ToLower()
                 .Contains(searchTerm));
         }
+
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return books.OrderBy(b => b.Id);
+
+            var orderQuery = OrderQueryBuilder
+                .CreateOrderQuery<Book>(orderByQueryString);
+
+            if(orderQuery is null)
+                return books.OrderBy(b=>b .Id);
+
+            return books.OrderBy(orderQuery);
+        }
     }
 }
+    
